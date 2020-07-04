a2fbd9751188d3e37c75d9e0241a039632913895
EricFerole <eferole@pixelcircus.ca>
2020-07-04 15:16:12 -0400

    Close to the solution

--- src/containers/selected_city.jsx
+++ src/containers/selected_city.jsx
@@ -4,20 +4,20 @@ import React, { Component } from 'react';
 class SelectedCity extends Component {
 
   render() {
-    console.log('SelectedCity:', this.props.selectedCity);
-    if (this.props.selectedCity === null) {
-      return (<p>Please select a city</p>);
-    }
-    const cities = this.props.cities.filter(element => element.name === this.props.id); // Return a list of arrays
+    // console.log('SelectedCity:', this.props.selectedCity);
+    // if (!this.props.selectedCity === null) {
+    //   return (<p>Please select a city</p>);
+    // }
+    const city = this.props.cities.filter(element => element.name === this.props.id); // Return a list of arrays
     console.log('selectedCityId:', this.props.id);
-    console.log('city:', cities);
+    console.log('City:', typeof city, city);
     // cities.filter('id === selectedCityIf');
+    console.log('test:', city);
 
     return (
       <div>
-        {console.log()}
-        {/* <h2>{this.props.selectCity.name}</h2>
-        <p>{this.props.selectCity.address}</p> */}
+        <h2>{city[0].name}</h2>
+        <p>{city[0].address}</p>
       </div>
     );
   }
